ff9b82ba8e8e3ba7355ea5f7254b548c120ebb53
Sebastian Pozoga <sebastian@pozoga.eu>
2018-08-14 04:00:30 +0200

    make SelfClean paraller

--- config/tasks/self/clean/networks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-#docker network rm $(docker network ls | awk '$3 == "bridge" && $2 != "bridge" { print $1 }')
-docker network prune -f
--- config/tasks/self/clean/volumes.sh
+++ config/tasks/self/clean/volumes.sh
@@ -2,5 +2,5 @@
 set -e
 
 # see: https://github.com/chadoe/docker-cleanup-volumes
-docker volume rm $(docker volume ls -qf dangling=true)
-docker volume ls -qf dangling=true | xargs -r docker volume rm
+# docker volume ls -qf dangling=true | xargs -r docker volume rm
+# WARNING: remove volumes can be unsafe
