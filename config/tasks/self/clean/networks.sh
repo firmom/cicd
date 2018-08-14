e565135d3cdef0ec597ba009804bd8bd0b82e2d0
Sebastian Pozoga <sebastian@pozoga.eu>
2018-08-14 03:38:12 +0200

    update clean docker networks script

--- config/tasks/self/clean/networks.sh
+++ config/tasks/self/clean/networks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
-docker network rm $(docker network ls | awk '$3 == "bridge" && $2 != "bridge" { print $1 }')
+#docker network rm $(docker network ls | awk '$3 == "bridge" && $2 != "bridge" { print $1 }')
+docker network prune -f
