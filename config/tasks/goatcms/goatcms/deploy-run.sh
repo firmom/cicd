fef5657be4e7d4c690ab647f5a720e585d426a39
Sebastian Pozoga <sebastian@pozoga.eu>
2018-08-14 02:18:13 +0200

    add goatcms log directory

--- config/tasks/goatcms/goatcms/deploy-run.sh
+++ config/tasks/goatcms/goatcms/deploy-run.sh
@@ -3,4 +3,5 @@ set -e
 
 killall goatcms-latest-dev || echo "Process is not exist"
 cd /data/serve/goatcms/goatcms
+mkdir -p /data/archive/logs/goatcms/
 /data/archive/goatcms/goatcms/goatcms-latest-dev run > /data/archive/logs/goatcms/goatcms.log 2>&1 &
