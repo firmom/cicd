67b53e9ca5014e8f4549e0144010dc59e5f4df8f
Sebastian Pozoga <sebastian@pozoga.eu>
2018-08-21 20:31:06 +0200

    add events.pozoga.eu prod deployment

--- config/tasks/sebastianpozoga/events.pozoga.eu/deploy.config.prod.sh
+++ config/tasks/sebastianpozoga/events.pozoga.eu/deploy.config.prod.sh
@@ -9,49 +9,60 @@ version: '3.4'
 services:
   events:
     image: spozoga/events.pozoga.eu
+    restart: always
+    networks:
+      - nproxy
+      - default
     environment:
       TZ: 'Europe/Warsaw'
-    restart: always
     environment:
+      - "VIRTUAL_HOST=events.pozoga.eu"
       - "DB_HOST=db"
       - "DB_PORT=3306"
-      - "DB_NAME=dev.events.pozoga.eu"
+      - "DB_NAME=prod.events.pozoga.eu"
       - "DB_USER=$EVENTS_POZOGA_EU_DB_USER"
-      - "DB_SNAPSHOT=$EVENTS_POZOGA_EU_DB_SNAPSHOT"
       - "DB_PASS=$EVENTS_POZOGA_EU_DB_PASS"
       - "WP_USER=$EVENTS_POZOGA_EU_WP_USER_NAME"
       - "WP_PASS=$EVENTS_POZOGA_EU_WP_USER_PASS"
       - "WP_EMAIL=$EVENTS_POZOGA_EU_WP_USER_EMAIL"
-      - "WP_HOST=$DEPLOY_DEV_REMOTE_HOST:2012"
+      - "WP_HOST=events.pozoga.eu"
       - "WP_TITLE=Events Poznań"
-      - "MIGRATE_FROM=http://events.pozoga.eu"
-      - "MIGRATE_TO=http://$DEPLOY_DEV_REMOTE_HOST:2012"
     volumes:
-      - "/dockerdata/sebastianpozoga/events.pozoga.eu/dev/uploads:/var/www/html/wp-content/uploads"
-    ports:
-      - 2012:80
+      - "/dockerdata/sebastianpozoga/events.pozoga.eu/prod/uploads:/var/www/html/wp-content/uploads"
+    expose:
+      - 80
   db:
     image: mariadb
     restart: always
+    networks:
+      - default
     environment:
-      - "MYSQL_DATABASE=dev.events.pozoga.eu"
+      - "MYSQL_DATABASE=prod.events.pozoga.eu"
       - "MYSQL_USER=$EVENTS_POZOGA_EU_DB_USER"
       - "MYSQL_PASSWORD=$EVENTS_POZOGA_EU_DB_PASS"
       - "MYSQL_ROOT_PASSWORD=$EVENTS_POZOGA_EU_DB_PASS"
     volumes:
-      - "/dockerdata/sebastianpozoga/events.pozoga.eu/dev/mysql:/var/lib/mysql"
+      - "/dockerdata/sebastianpozoga/events.pozoga.eu/prod/mysql:/var/lib/mysql"
   phpmyadmin:
     image: phpmyadmin/phpmyadmin
     restart: always
+    networks:
+      - default
     ports:
-     - 2013:80
+     - 2014:80
     environment:
       - "PMA_HOST=db"
-      - "PMA_VERBOSE=dev.events.pozoga.eu"
+      - "PMA_VERBOSE=prod.events.pozoga.eu"
       - "PMA_PORT=3306"
       - "PMA_ARBITRARY=1"
 
+networks:
+  nproxy:
+    external:
+      name: nproxy
+  default:
+
 EndOfMessage
 
-ssh $DEPLOY_DEV_REMOTE_USER@$DEPLOY_DEV_REMOTE_HOST 'mkdir -p ~/sebastianpozoga/events.pozoga.eu-dev/'
-scp -o "StrictHostKeyChecking no" /data/docker/sebastianpozoga/events.pozoga.eu-dev/docker-compose.yaml $DEPLOY_DEV_REMOTE_USER@$DEPLOY_DEV_REMOTE_HOST:~/sebastianpozoga/events.pozoga.eu/docker-compose.yaml
+ssh $DEPLOY_DEV_REMOTE_USER@$DEPLOY_DEV_REMOTE_HOST 'mkdir -p ~/sebastianpozoga/events.pozoga.eu-prod/'
+scp -o "StrictHostKeyChecking no" /data/docker/sebastianpozoga/events.pozoga.eu-prod/docker-compose.yaml $DEPLOY_DEV_REMOTE_USER@$DEPLOY_DEV_REMOTE_HOST:~/sebastianpozoga/events.pozoga.eu/docker-compose.yaml
