8e496b7a17bafe43c6b38f0942eba884f3252802
Sebastian Pozoga <sebastian@pozoga.eu>
2018-10-01 21:59:52 +0200

    correct golang tests for goatcms/goatcms

--- config/helpers/golang/test.sh
+++ config/helpers/golang/test.sh
@@ -4,9 +4,9 @@ set -e
 HOST="$1"
 REPO="$2"
 TAG="$3"
+TEST_PATH="$4"
 GOPATH="/data/code/$REPO-$TAG"
 SRC_PATH="$GOPATH/src/$HOST/$REPO"
 
 cd $SRC_PATH
-rm -rf vendor
-dep ensure
+go test $TEST_PATH
