843a50702de5d941ca2300a5b30e3ca8bc77047f
Sebastian Pozoga <sebastian@pozoga.eu>
2018-10-14 12:22:29 +0200

    correct webslots domain

--- config/tasks/goatcms/webslots/prod/prod.deploy.config.sh
+++ config/tasks/goatcms/webslots/prod/prod.deploy.config.sh
@@ -23,7 +23,7 @@ services:
     volumes:
       - "/dockerdata/$REPO/$TAG/uploads:/app/wp-content/uploads"
       - "/dockerdata/$REPO/$TAG/snapshots:/data/snapshots"
-      - "/dockerdata/certs/goatcms.org:/certs"
+      - "/dockerdata/certs/firmom.com:/certs"
     ports:
       - 4333:443
   db:
