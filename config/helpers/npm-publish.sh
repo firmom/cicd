d24b67629cbd7917ede7b6ac6834f13f779efbd9
Sebastian Pozoga <sebastian@pozoga.eu>
2018-10-21 21:13:29 +0200

    npm-publish get version from npm repo now

--- config/helpers/npm-publish.sh
+++ config/helpers/npm-publish.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 set -e
-
 ACCESS="$1"
 
 cat > ~/.npmrc << EndOfMessage
 //registry.npmjs.org/:_authToken=$NPM_TOKEN
 EndOfMessage
 
+PACKAGE_NAME=$(cat package.json \
+  | grep name \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+LATEST_VERSION=$(npm show $PACKAGE_NAME version)
+
+npm version $LATEST_VERSION
+npm version --no-git-tag-version --allow-same-version $LATEST_VERSION
 npm version patch --no-git-tag-version
+
 npm publish --access $ACCESS
