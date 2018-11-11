a84fc03eceaea7d6cbd4db4fe1d2cb6b1166ce12
Sebastian Pozoga <sebastian@pozoga.eu>
2018-11-11 17:56:41 +0100

    bugfixes and add help to devtools desktop

--- config/helpers/devtools/deploy-single-devtools.sh
+++ config/helpers/devtools/deploy-single-devtools.sh
@@ -22,11 +22,34 @@ PORT_SFTP=$(($BASE_PORT + 6))
 DEST_DIR_PATH="/data/deploy/_user/$name"
 DEST_FILE_PATH="$DEST_DIR_PATH/docker-compose.yaml"
 
+DEST_HELP_DIR_PATH="/data/deploy/_user/$name"
+DEST_HELP_FILE_PATH="$DEST_HELP_DIR_PATH/README_PORTS.txt"
+
+REMOTE_DIR_PATH="~/_user/$name"
+REMOTE_FILE_PATH="$DEST_DIR_PATH/docker-compose.yaml"
+
+REMOTE_HELP_DIR_PATH="/dockerdata/devtools/$NAME/work"
+REMOTE_HELP_FILE_PATH="$DEST_HELP_DIR_PATH/README_PORTS.txt"
+
 mkdir -p $DEST_DIR_PATH
 
-# Add config file
+# Prepare config file
 cat > $DEST_FILE_PATH << EndOfMessage
 version: '3'
+
+## $NAME port list:
+# (public port) -> (container port)
+# $PORT_VNC_SYS -> 5000 (VNC system port)
+# $PORT_VNC_CONN -> 5901 (VNC first desktop - use it for vnc viewer )
+# $PORT_VNC_WEB -> 6901 (VNC web browser port)
+# $PORT_80 -> 80 (default HTTP port)
+# $PORT_8080 -> 8080 (external HTTP port)
+# $PORT_3000 -> 3000 (development port)
+# $PORT_SFTP -> 22 (SFTP port to share directory)
+#
+# Your personal data (never share):
+# username: $NAME
+# password: $PASSWORD
 
 services:
   centos:
@@ -35,7 +58,7 @@ services:
      - "$PORT_VNC_SYS:5000"
      - "$PORT_VNC_CONN:5901"
      - "$PORT_VNC_WEB:6901"
-     - "$PORT_80:3000"
+     - "$PORT_80:80"
      - "$PORT_8080:8080"
      - "$PORT_3000:3000"
     volumes:
@@ -60,6 +83,35 @@ services:
 
 EndOfMessage
 
-cd $DEST_DIR_PATH
+# Prepare help file
+cat > $DEST_HELP_FILE_PATH << EndOfMessage
+## $NAME port list:
+# (public port) -> (container port)
+# $PORT_VNC_SYS -> 5000 (VNC system port)
+# $PORT_VNC_CONN -> 5901 (VNC first desktop - use it for vnc viewer )
+# $PORT_VNC_WEB -> 6901 (VNC web browser port)
+# $PORT_80 -> 80 (default HTTP port)
+# $PORT_8080 -> 8080 (external HTTP port)
+# $PORT_3000 -> 3000 (development port)
+# $PORT_SFTP -> 22 (SFTP port to share directory)
+EndOfMessage
+
+# Deploy config
+ssh $DEPLOY_DEVTOOLS_REMOTE_USER@$DEPLOY_DEVTOOLS_REMOTE_HOST "mkdir -p $REMOTE_DIR_PATH"
+scp -o "StrictHostKeyChecking no" "$DEST_FILE_PATH" "$DEPLOY_DEVTOOLS_REMOTE_USER@$DEPLOY_DEVTOOLS_REMOTE_HOST:$REMOTE_FILE_PATH"
+
+# Deploy help
+ssh $DEPLOY_DEVTOOLS_REMOTE_USER@$DEPLOY_DEVTOOLS_REMOTE_HOST "mkdir -p $REMOTE_HELP_DIR_PATH"
+scp -o "StrictHostKeyChecking no" "$DEST_HELP_FILE_PATH" "$DEPLOY_DEVTOOLS_REMOTE_USER@$DEPLOY_DEVTOOLS_REMOTE_HOST:$REMOTE_HELP_FILE_PATH"
+
+# Append app
+ssh -o "StrictHostKeyChecking no" $DEPLOY_DEVTOOLS_REMOTE_USER@$DEPLOY_DEVTOOLS_REMOTE_HOST << ENDSSH
+#commands to run on remote host
+set -e
+
+docker pull $IMAGE
+cd $REMOTE_DIR_PATH
 docker-compose rm --force --stop -v
 docker-compose up -d
+
+ENDSSH
