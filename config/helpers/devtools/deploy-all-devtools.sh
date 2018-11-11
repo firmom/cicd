15d1eadd20a2f3df56b357b6e613a1bc07d9d067
Sebastian Pozoga <sebastian@pozoga.eu>
2018-11-11 18:27:40 +0100

    correct bash if

--- config/helpers/devtools/deploy-all-devtools.sh
+++ config/helpers/devtools/deploy-all-devtools.sh
@@ -13,11 +13,13 @@ for i in `env | grep -E "^USER_.*_USERNAME="`; do
   eval email=\${${baseKey}EMAIL}
   eval roles=\${${baseKey}ROLSE}
   eval password=\${${baseKey}PASSWORD}
-  eval github=\${${baseKey}_CONNECT_GITHUB}
-  eval user_base_port=\${${baseKey}_BASE_PORT}
-  eval vnc_resolution=\${${baseKey}_VNC_RESOLUTION}
-  eval vnc_col_depth=\${${baseKey}_VNC_COL_DEPTH}
-  baseport=$(($user_base_port + $PORT_OFFSET))
+  eval github=\${${baseKey}CONNECT_GITHUB}
+  eval user_base_port=\${${baseKey}BASE_PORT}
+  eval vnc_resolution=\${${baseKey}VNC_RESOLUTION}
+  eval vnc_col_depth=\${${baseKey}VNC_COL_DEPTH}
 
-  /app/config/helpers/devtools/deploy-single-devtools.sh $IMAGE $username $password $vnc_resolution $vnc_col_depth $email $firstname $lastname $base_port
+  if [[ ! -z $user_base_port && ! -z $PORT_OFFSET ]]; then
+    baseport=$(($user_base_port + $PORT_OFFSET))
+    /app/config/helpers/devtools/deploy-single-devtools.sh $IMAGE $username $password $vnc_resolution $vnc_col_depth $email $firstname $lastname $base_port
+  fi
 done
